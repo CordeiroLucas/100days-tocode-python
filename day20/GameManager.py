bd5d0c2d926235ad43a1ccdf41d937a92556cc0b
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-17 20:03:07 -0300

    feat: Food Collision Functions

--- day20/GameManager.py
+++ day20/GameManager.py
@@ -1,16 +1,20 @@
+from turtle import Screen
+
 from Snake import *
 from Food import *
-from turtle import Screen
+
 import time
+from random import randrange
 
 class GameManager:
+	screen = Screen()
+	snake = Snake()
+	food = Food()
+
+
 	def __init__(self):
 		self.score = 0
 		self.game_is_on = True
-
-		self.screen = Screen()		
-		self.snake = Snake()
-		self.food = Food()
 
 		self.__setup_screen()
 
@@ -31,14 +35,16 @@ class GameManager:
 
 
 	def run(self):
+		self.__respawn_food()
+		
 		while self.game_is_on:
 			
 			self.screen.update()
 			time.sleep(0.1) ## 100ms -> 10fps
 
 			self.snake.move()
-
 			self.borders_colision()
+			self.check_food()
 
 		self.screen.exitonclick()
 	
@@ -55,8 +61,25 @@ class GameManager:
 
 		if head.xcor() > max_x or head.ycor() > max_y or head.xcor() < max_x*-1 or head.ycor() < max_y*-1:
 			self.__gameOver()
-		
-			
 
-			
+	def __respawn_food(self):
+		screen_width = self.screen.window_width()
+		screen_height = self.screen.window_height()
+
+		max_x = int(screen_width/2)
+		max_y = int(screen_height/2)
+
+		new_x = randrange(max_x * -1, max_x, 20)
+		new_y = randrange(max_y * -1, max_y, 20)
+
+		new_pos = (new_x, new_y)
+
+		self.food.spawn(new_pos)
+
+	def check_food(self):
+		if self.food.get_pos() == self.snake.head.pos():
+			self.__respawn_food()
+			self.score += 1
+			print(self.score)
+			# self.snake.eat()
 		
