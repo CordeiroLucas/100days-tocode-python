3147c8fbb474733db47243628f9e8b015a5deeba
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-18 19:49:35 -0300

    cleanup: scoreboard consts

--- day20/ScoreBoard.py
+++ day20/ScoreBoard.py
@@ -1,5 +1,8 @@
 from turtle import Turtle
 
+FONT = ("Arial", 14, "bold")
+ALIGNMENT = "center"
+
 class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
@@ -10,8 +13,9 @@ class ScoreBoard(Turtle):
         self.goto(0, 270)
         
     def update_scoreboard(self):
-        self.clear()
-        self.write(f"Score: {self.score}", align="center", font=("Arial", 14, "bold"))
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
 
     def increase_score(self):
+        self.clear()
         self.score += 1
+        self.update_scoreboard()
