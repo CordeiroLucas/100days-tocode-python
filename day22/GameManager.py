c478ddc557d6ccc05858094978c14c77319011fb
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-21 10:03:46 -0300

    feat: Paddle Control

--- day22/GameManager.py
+++ day22/GameManager.py
@@ -4,19 +4,24 @@ from objects.ScoreBoard import *
 from turtle import Turtle, Screen
 
 class GameManager:
-    screen = Screen()
-    ball = Ball()
-    player = Paddle(True)
-    enemy = Paddle()
-    ScoreBoard = ScoreBoard()
-    def __init__(self):
-        self.screen.bgcolor("black")
-        self.screen.title("Pong Game")
-        self.screen.setup(800, 600)
-        self.screen.exitonclick()
-    
-    def run(self):
-        print(self.player.isPlayer)
-        print(self.enemy.isPlayer)
-    
-    
+	screen = Screen()
+	ball = Ball()
+	player = Paddle(True)
+	enemy = Paddle()
+	ScoreBoard = ScoreBoard()
+
+	screen.listen()
+	screen.onkeypress(player.up, "Up")
+	screen.onkeypress(player.down, "Down")
+
+	def __init__(self):
+		self.screen.bgcolor("black")
+		self.screen.title("Pong Game")
+		self.screen.setup(800, 600)
+		self.screen.exitonclick()
+	
+	def run(self):
+		print(self.player.isPlayer)
+		print(self.enemy.isPlayer)
+	
+		pass
