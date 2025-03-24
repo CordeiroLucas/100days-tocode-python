effc0982075077cb960de78408f094a067778316
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-24 20:55:55 -0300

    feat: Ball Movement Basics

--- day22/objects/Ball.py
+++ day22/objects/Ball.py
@@ -3,6 +3,12 @@ from turtle import Turtle
 class Ball(Turtle):
     def __init__(self, shape = "circle"):
         super().__init__(shape)
-        self.shapesize(0.7, 0.7)
+        self.screen.tracer(0)
         self.penup()
         self.color("white")
+        self.right(-45)
+    
+    def move(self):
+        new_x = self.xcor() + 10
+        new_y = self.ycor() + 10
+        self.goto(new_x, new_y)
