8d2b5c9e879004ba06201c8d517cecca734cfc63
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-24 20:57:14 -0300

    fix: update issues

--- day22/GameManager.py
+++ day22/GameManager.py
@@ -1,29 +1,33 @@
 from objects.Ball import *
 from objects.Paddle import *
 from objects.ScoreBoard import *
-from turtle import Turtle, Screen
+from turtle import Screen
+import time
 
 class GameManager:
 	screen = Screen()
 	ball = Ball()
 	l_paddle = Paddle((-350, 0))
 	r_paddle = Paddle((350, 0))
-	ScoreBoard = ScoreBoard()
+	# ScoreBoard = ScoreBoard()
 
 	screen.listen()
 	screen.onkeypress(l_paddle.up, "w")
 	screen.onkeypress(l_paddle.down, "s")
-
+	
 	screen.onkeypress(r_paddle.up, "Up")
 	screen.onkeypress(r_paddle.down, "Down")
 
 	def __init__(self):
+		self.screen.tracer(0)
 		self.screen.bgcolor("black")
 		self.screen.title("Pong Game")
 		self.screen.setup(800, 600)
-		self.screen.exitonclick()
 		self.game_is_on = True
 	
 	def run(self):
 		while(self.game_is_on):
+			self.ball.move()
 			self.screen.update()
+			time.sleep(0.05)
+		self.screen.exitonclick()
