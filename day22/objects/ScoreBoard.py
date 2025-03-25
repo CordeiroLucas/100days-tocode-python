a010c75592e2f5784fa297b10209e4262267efe6
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-03-25 19:51:24 -0300

    feat: ScoreBoard Basics

--- day22/objects/ScoreBoard.py
+++ day22/objects/ScoreBoard.py
@@ -1,8 +1,26 @@
 from turtle import Turtle
 
+FONT = ("Sans", 32, "normal")
+
 class ScoreBoard(Turtle):
-    def __init__(self):
-        super().__init__()
-        self.penup()
-        self.color("white")
-        self.score = 0
+	score_p0:int = 0
+	score_p1:int = 0
+	def __init__(self):
+		super().__init__()
+		self.screen.tracer(0)
+		self.hideturtle()
+		self.penup()
+		self.color("white")
+		self.score = 0
+		self.goto(0,240)
+
+	def display(self):
+		self.clear()
+		self.write(f"{self.score_p0} - {self.score_p1}",align="center", font=FONT)
+
+	def point(self, player):
+		match (player):
+			case 0:
+				self.score_p0 += 1
+			case 1:
+				self.score_p1 += 1
