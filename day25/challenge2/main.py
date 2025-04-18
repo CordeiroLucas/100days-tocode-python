7dea44217f90c6491df17a919df9ef24f9ec2a3c
CordeiroLucas <lcp2@cesar.school>
2025-04-18 19:23:08 -0300

    fix: storing missing states

--- day25/challenge2/main.py
+++ day25/challenge2/main.py
@@ -15,16 +15,20 @@ turtle.hideturtle()
 #################
 total_states = data.count().state
 current_score = 0
+
+states_guessed = []
+missing_states = []
+
+
 game_is_on = True
 while game_is_on:
 	guess = screen.textinput(f"{current_score}/50 | Guess the US States", "Type a State")
 	
 	all_states = data.state.to_list()
-	states_guessed = []
+	
 
 	if guess in all_states:
 		index_found = data.loc[data.state == guess].index[0] # Search By State Name and Return its Index
-		print(index_found)
 
 		state_coord = (float(data.x[index_found]), float(data.y[index_found]))
 
@@ -33,21 +37,28 @@ while game_is_on:
 
 		current_score+=1
 		states_guessed.append(data.state[index_found])
+		print(data.state[index_found])
+		continue
 
 	if guess.lower() == "exit":
 		print("Saindo do Programa")
-
-		missing_states = []
-		for state in all_states:
-			if state not in states_guessed:
-				missing_states.append(state)
 		break
 
 	if guess not in all_states:
 		print("Errouu")
 		game_is_on = False
+		break
 
 ###################
+
+print(states_guessed)
+
+for state in all_states:
+	if state not in states_guessed:
+		missing_states.append(state)
+
+new_data = pd.DataFrame(missing_states)
+new_data.to_csv("states_to_learn.csv")
 
 print(missing_states)
 
