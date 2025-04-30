6692dcf5a0d2c28a60a47852cc4f9c7da36a57ff
CordeiroLucas <pacheco.lucascordeiro@gmail.com>
2025-04-30 18:06:11 -0300

    feat: tk interface basics

--- day27/funcTests.py
+++ day27/funcTests.py
@@ -14,3 +14,13 @@ def add(*args):
     return sum        
 
 print(add(1,2,3,4,5,6), add(1,2,3), add(1,2,3,4,5,6,7,8,9,10))
+
+# Python Keyword Arguments
+
+def calculate(n, **kwargs):
+    print(kwargs)
+    n+= kwargs.get("add")
+    n*= kwargs.get("multiply")
+    return n
+
+print(calculate(2, add=3, multiply=5))  # 25
--- day27/tkTests.py
+++ day27/tkTests.py
@@ -1,11 +1,27 @@
 import tkinter
+from tkinter import Button
+
+# Button
+def button_clicked():
+    print("Clicked")
+    my_label.config(text=entry.get())
+
 
 window = tkinter.Tk()
-window.title("My First GUI Program")
+window.title("Mile Converter")
 window.minsize(width=500, height=300)
 
 # Label
-my_label = tkinter.Label(text="Hello World", font=("Arial", 24, "bold"))
+my_label = tkinter.Label(text="I am a Label", font=("Arial", 24, "bold"))
 my_label.pack()
+
+my_label["text"] = "I am a label"
+my_label.config(text="Config")
+
+entry = tkinter.Entry(width=10)
+entry.pack()
+
+button = Button(text="click me", command=button_clicked)
+button.pack()
 
 window.mainloop()
